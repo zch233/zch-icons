4b633c77ecb158f4094e4e8cfb263bb08d7fd0e2
zhangchao <529743595@qq.com>
2022-06-17 00:24:12 +0800

    feat: add naive-ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 import { defineNuxtConfig } from 'nuxt';
+import Components from 'unplugin-vue-components/vite';
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
@@ -15,6 +17,22 @@ export default defineNuxtConfig({
             link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
         },
     },
+    vite: {
+        plugins: [
+            Components({
+                resolvers: [NaiveUiResolver()], // Automatically register all components in the `components` directory
+            }),
+        ],
+        // @ts-expect-error: Missing ssr key
+        ssr: {
+            noExternal: ['moment', 'icon-vue3', 'naive-ui', '@juggle/resize-observer', '@css-render/vue3-ssr'],
+        },
+        optimizeDeps: {
+            include: ['date-fns-tz/esm/formatInTimeZone'],
+        },
+    },
     css: ['@/assets/styles/reset.css', '@/assets/styles/global.less'],
-    build: {},
+    build: {
+        transpile: ['naive-ui', 'vueuc', '@css-render/vue3-ssr', '@juggle/resize-observer'],
+    },
 });
---
+++ pages/icons/index.vue
@@ -0,0 +1,99 @@
+<template>
+    <div class="icons">
+        <div class="icons-header">
+            <IconSearcher />
+        </div>
+        <div class="icons-main">
+            <NTabs type="segment">
+                <NTabPane name="outlined" tab="Outlined">
+                    <h1 class="icons-main-title">999 Icons</h1>
+                    <div
+                        v-for="item in 100"
+                        :key="item"
+                        style="
+                            display: inline-flex;
+                            margin: 8px;
+                            align-items: center;
+                            justify-content: center;
+                            width: 120px;
+                            height: 120px;
+                            border-radius: 8px;
+                            background-color: #fff;
+                        "
+                    >
+                        <IconBookFilled size="32" />
+                    </div>
+                </NTabPane>
+                <NTabPane name="filled" tab="Filled">
+                    <h1 class="icons-main-title">998 Icons</h1>
+                </NTabPane>
+                <NTabPane name="twoTone" tab="TwoTone">
+                    <h1 class="icons-main-title">996 Icons</h1>
+                </NTabPane>
+                <NTabPane name="colorful" tab="Colorful">
+                    <h1 class="icons-main-title">994 Icons</h1>
+                </NTabPane>
+            </NTabs>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { IconBookFilled } from 'icon-vue3';
+
+definePageMeta({
+    title: 'Icons',
+});
+</script>
+
+<style scoped lang="less">
+.icons {
+    background-color: #f0f1f3;
+    &-header {
+        padding-top: 32px;
+        background-color: #fff;
+        &-tab {
+            width: 80vw;
+            margin: 0 auto;
+            display: flex;
+            padding-bottom: 40px;
+            &-item {
+                border: 2px solid #f0f1f3;
+                border-radius: 8px;
+                font-size: 16px;
+                padding: 15px 24px;
+                cursor: pointer;
+                &:hover {
+                }
+            }
+        }
+    }
+    &-main {
+        //padding: 32px 0;
+        //width: 86vw;
+        margin: 0 auto;
+        /deep/.n-tabs {
+            .n-tabs-nav {
+                background-color: #fff;
+                padding-bottom: 40px;
+            }
+            .n-tabs-rail {
+                margin-left: 8vw;
+                width: 660px;
+            }
+            .n-tabs-tab {
+                padding: 15px 24px;
+                font-size: 16px;
+            }
+            .n-tab-pane {
+                margin: 20px auto 32px;
+                width: 86vw;
+            }
+        }
+        &-title {
+            font-size: 20px;
+            margin-bottom: 24px;
+        }
+    }
+}
+</style>
