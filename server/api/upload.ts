f6d8c48fbe1c15cf0b2f6a5e29692d97687c89cb
zhangchao <529743595@qq.com>
2022-06-23 11:37:56 +0800

    feat: update upload api

--- server/api/upload.ts
+++ server/api/upload.ts
@@ -1,16 +1,39 @@
 import formidable from 'formidable';
+import fs from 'fs';
+import path from 'path';
 
-const form = formidable({ multiples: true });
+const svgDir = 'svg';
+
+const form = formidable({ multiples: true, uploadDir: svgDir, keepExtensions: true });
 
 export default defineEventHandler(async event => {
-    // await new Promise((resolve, reject) => {
-    //     form.parse(event.event.req, async (err, fields, files) => {
-    //         if (err) throw err;
-    //         resolve({category: fields.category, files})
-    //         console.log(files);
-    //     });
-    // })
-    const formData = await form.parse(event.event.req);
-    console.log(formData);
+    await createDir(svgDir);
+    await new Promise((resolve, reject) => {
+        form.parse(event.event.req, async (err, fields, files) => {
+            if (err) throw err;
+            const category = fields.category?.[0];
+            const svgFiles = files.file as formidable.File[];
+            await createDir(`${svgDir}/${category}`);
+            const generatePath = (fileName: string, categoryDir?: string) => path.resolve('./svg', categoryDir || '', fileName);
+            Promise.all(
+                svgFiles.map(
+                    ({ originalFilename, newFilename }) =>
+                        new Promise(() => fs.renameSync(generatePath(newFilename), generatePath(originalFilename!, category)))
+                )
+            ).catch(err => {
+                throw err;
+            });
+            resolve({ category: fields.category, files });
+        });
+    });
     return { code: 200, message: 'success', data: null };
 });
+
+export const createDir = async (dir: string) => {
+    const iconsDir = path.join('./', dir);
+    try {
+        await fs.accessSync(iconsDir);
+    } catch (err) {
+        await fs.mkdirSync(iconsDir);
+    }
+};
