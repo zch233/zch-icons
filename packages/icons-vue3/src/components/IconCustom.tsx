2847954013bfeb07268aa7133a85539180771c22
zhangchao <529743595@qq.com>
2022-07-04 20:51:57 +0800

    fix: fix IconCustom component

--- packages/icons-vue3/src/components/IconCustom.tsx
+++ packages/icons-vue3/src/components/IconCustom.tsx
@@ -1,12 +1,13 @@
 import { computed, FunctionalComponent } from 'vue';
 import Icon from './Icon';
 
-export const IconCustom = (): FunctionalComponent<{ iconName: string }> => {
+export const IconCustom: FunctionalComponent<{ iconName: string }> = (props, context) => {
+    console.log(props);
     const icon = computed(async () => {
         const aa = await import('../../../../assets/svg/empty.svg');
         console.log(aa);
         return aa.default;
     });
     // @ts-ignore
-    return (props, context) => <Icon icon={icon.value} />;
+    return <Icon icon={icon.value} />;
 };
