9cd7ae42fc88107149a67ddee2570de998945aea
zhangchao <529743595@qq.com>
2022-07-04 20:59:57 +0800

    feat: update version

--- packages/icons-vue3/src/components/IconCustom.tsx
+++ packages/icons-vue3/src/components/IconCustom.tsx
@@ -4,7 +4,7 @@ import Icon from './Icon';
 export const IconCustom: FunctionalComponent<{ iconName: string }> = (props, context) => {
     console.log(props);
     const icon = computed(async () => {
-        const aa = await import('../../../../assets/svg/empty.svg');
+        const aa = await import('../../../../src/assets/svg/empty.svg');
         console.log(aa);
         return aa.default;
     });
