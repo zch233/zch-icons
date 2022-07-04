9f79699a88c5fdd4ad1e997dd0f9ef50de86daa2
zhangchao <529743595@qq.com>
2022-07-04 17:20:20 +0800

    feat: test icon custom

--- packages/icons-vue3/src/components/IconCustom.tsx
+++ packages/icons-vue3/src/components/IconCustom.tsx
@@ -1,12 +1,12 @@
 import { computed, FunctionalComponent } from 'vue';
 import Icon from './Icon';
 
-export const IconCustom = (): FunctionalComponent<{ icon: string }> => {
+export const IconCustom = (): FunctionalComponent<{ iconName: string }> => {
     const icon = computed(async () => {
-        const aa = await import('../../assets/svg/empty.svg?raw');
+        const aa = await import('../../../../assets/svg/empty.svg');
         console.log(aa);
         return aa.default;
     });
     // @ts-ignore
-    return (props, context) => <Icon icon={icon} />;
+    return (props, context) => <Icon icon={icon.value} />;
 };
